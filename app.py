f05517d829b3f324f4750471330489d9c69cece3
rohitsahu70 <89459208+rohitsahu70@users.noreply.github.com>
2021-12-25 16:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def about():
 
 def Project():
 	st.write(
-		'''Please Click on the Below link for reaching out the project :                                                                                    
-		Read More :point_right: https://face-recognition-attendance-system.epizy.com/.
+		'''Please Click on the Below link for reaching out the project :           
+		Read More :point_right: https://face-recognition-attendance-system.epizy.com/
 		''')
 
 def main():
